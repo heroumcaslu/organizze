30ef6cfe1c24fc023f8376606d9d9813aa69b7ba
Lucas H. Silva <lucas_herom@hotmail.com>
2020-04-13 13:44:47 -0300

    social proof

--- index.php
+++ index.php
@@ -57,6 +57,7 @@ $configSiteName = "Organizze";
             require "{$configThemePath}/offer.php";
             require "{$configThemePath}/callToAction.php";
             require "{$configThemePath}/functionalities.php";
+            require "{$configThemePath}/socialProof.php";
 
         }
 
---
+++ theme/socialProof.php
@@ -0,0 +1,28 @@
+<section class="social-proof">
+    <div class="container">
+        <header class="offer-header">
+            <h2>A opinião de quem realmente importa</h2>
+            <div class="line-shape"></div>
+        </header>
+        <div class="comments flex">
+            <div class="comment flex-3">
+                <img class="rounded" src="<?= $configBase ?>/assets/img/user-img.jpg" alt="">
+                <p>O homem quando está com o seu @organizze organizado não quer guerra com ninguém! Começart dezembro com tudo controlado!</p>
+                <span class="user-name">Lucas</span>
+                <a class="instagram-user" href="https://www.instagram.com/heroumcaslu">@heroumcaslu</a>
+            </div>
+            <div class="comment flex-3">
+                <img class="rounded" src="<?= $configBase ?>/assets/img/user-img.jpg" alt="">
+                <p>O homem quando está com o seu @organizze organizado não quer guerra com ninguém! Começart dezembro com tudo controlado!</p>
+                <span class="user-name">Lucas</span>
+                <a class="instagram-user" href="https://www.instagram.com/heroumcaslu">@heroumcaslu</a>
+            </div>
+            <div class="comment flex-3">
+                <img class="rounded" src="<?= $configBase ?>/assets/img/user-img.jpg" alt="">
+                <p>O homem quando está com o seu @organizze organizado não quer guerra com ninguém! Começart dezembro com tudo controlado!</p>
+                <span class="user-name">Lucas</span>
+                <a class="instagram-user" href="https://www.instagram.com/heroumcaslu">@heroumcaslu</a>
+            </div>
+        </div>
+    </div>
+</section>
